7c3c71052058da81e32cff72d109caafba234d5f
valmir augusto dos santos <valmirdeveloper@gmail.com>
2023-06-20 19:13:57 -0300

    adicionei a aula 017

---
+++ 017-eventos de mouse/script.js
@@ -0,0 +1,50 @@
+// Apertou o mouse
+
+function apertouOmouse(){
+    console.log("O mouse foi apertado!");
+}
+
+
+// Mouse up
+
+function solteiOmouse(){
+    console.log("Soltei o mouse!");
+}
+
+
+// Mouse Over
+
+function passouOmouse(){
+    console.log("Você passou o mouse em cima do botão!");
+}
+
+
+// Mouse out
+
+function tirouOmouse(){
+    console.log("Você tirou o mouse do botão!");
+}
+
+
+// Mouse move
+
+function movendoOmouse(){
+    console.log("Você está mexendo o mouse sobre o botão!");
+}
+
+
+// On Click
+
+function onClick(){
+    console.log("Você clicou no botão");
+}
+
+
+// Context menu
+
+
+function botaoDireito(){
+    console.log("Você clicou no botão direito do mouse!");
+
+    return false;
+}
